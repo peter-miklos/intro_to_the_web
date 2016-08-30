16aea5ff9573691e7c249807723a2ffee893d2bd
Stéphan Gaud <stephgoeseast@gmail.com>
2016-08-30 15:58:39 +0100

    add cat pic

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 set :session_secret, 'super secret'
+
 get '/' do
    "Hello"
 end
 
 get '/secret' do
   "secret page"
+end
+
+get '/cat' do
+"<div style='border: 3px dashed  red'>
+<img src='http://bit.ly/1eze8aE'>
+</div>
+"
 end
